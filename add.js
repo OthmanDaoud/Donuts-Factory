18cf5e5f637a4e3ddcc844e847b0554b1d099bae
unknown <o.daoued@gmail.com>
2024-06-05 15:33:25 +0300

    Donuts Factory

--- add.js
+++ add.js
@@ -1,22 +1,25 @@
 let name = prompt("Please Enter Your Name");
 
-let gender = prompt("Please Enter Your Gender (male/female)");
-if (gender == "male") {
-  alert("Welcome Mr. " + name);
-} else if (gender == "female") {
-  alert("Welcome Ms. " + name);
-} else {
-  while (gender != "male" || gender != "female") {
-    gender = prompt("Please Enter Your Gender (male/female)");
-    if (gender == "male") {
-      alert("Welcome Mr. " + name);
-      break;
-    } else if (gender == "female") {
-      alert("Welcome Ms. " + name);
-      break;
+function quastion() {
+  let gender = prompt("Please Enter Your Gender (male/female)");
+  if (gender == "male") {
+    alert("Welcome Mr. " + name);
+  } else if (gender == "female") {
+    alert("Welcome Ms. " + name);
+  } else {
+    while (gender != "male" || gender != "female") {
+      gender = prompt("Please Enter Your Gender (male/female)");
+      if (gender == "male") {
+        alert("Welcome Mr. " + name);
+        break;
+      } else if (gender == "female") {
+        alert("Welcome Ms. " + name);
+        break;
+      }
     }
   }
 }
+quastion();
 
 let order = prompt(
   "Do you want to order (Donut, Coffee, Ice creame or Bakery) (yes/no)?"
